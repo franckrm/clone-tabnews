7bf9df9f693e522a5410540ff17e09b40b63d491
franckrm <74570638+franckrm@users.noreply.github.com>
2025-01-23 20:52:52 +0000

    wip: refactor status api

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,10 @@
 test("GET to /api/v1/status should be 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+
+  const responseBody = await response.json();
+  expect(responseBody.updated_at).toBeDefined();
+
+  const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
+  expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 });
