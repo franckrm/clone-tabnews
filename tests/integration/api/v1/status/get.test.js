3ea7d0baadc8070c633a57936fd00fea1f29f1b1
franckrm <74570638+franckrm@users.noreply.github.com>
2025-01-29 15:52:59 +0000

    wip: public api status

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -3,10 +3,10 @@ test("GET to /api/v1/status should be 200", async () => {
   expect(response.status).toBe(200);
 
   const responseBody = await response.json();
-  expect(responseBody.updated_at).toBeDefined();
 
   const parsedUpdateAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdateAt);
 
   expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.max_connections).toEqual(100);
 });
