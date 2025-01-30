ea5283ead1a8b230007f529daee00a72bc9f89e9
franckrm <74570638+franckrm@users.noreply.github.com>
2025-01-30 22:47:47 +0000

    wip: add opened connections in api\status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -13,12 +13,20 @@ async function status(request, response) {
   const databaseMaxConnetionsValue =
     databaseMaxConnectionsResult.rows[0].max_connections;
 
+  const databaseOpenedConnetionsResult = await database.query(
+    "select count(*)::int from pg_stat_activity where datname = 'local_db'",
+  );
+
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnetionsResult.rows[0].count;
+
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnetionsValue),
+        opened_connections: databaseOpenedConnectionsValue,
       },
     },
   });
