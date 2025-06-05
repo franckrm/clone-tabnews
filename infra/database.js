f247b2b2cdbb748586f18bdd179278fe69a2557f
Ribeirovisk <74570638+franckrm@users.noreply.github.com>
2025-06-05 10:59:32 +0000

    fix: conect to database with string conection

--- infra/database.js
+++ infra/database.js
@@ -16,11 +16,7 @@ async function query(queryObject) {
 
 async function getNewClient() {
   const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    connectionString: process.env.DATABASE_URL,
     ssl: getSSLValues(),
   });
 
